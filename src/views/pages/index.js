2aaa965aa325ec4069a0a8eb49090d629e20a8d1
wladengine <engine@notebook>
2022-11-07 10:20:33 +0300

    Turned back project templates, added start page, added navigation

---
+++ src/views/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard'
+import DocumentList from './DocumentList'
+import FeatureList from './FeatureList'
+import ParameterList from './ParameterList'
+
+export { Dashboard, DocumentList, FeatureList, ParameterList }
